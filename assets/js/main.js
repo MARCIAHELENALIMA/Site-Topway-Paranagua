434229b8fc9b7b1007346804d3d976b289893665
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-01-13 15:58:01 -0300

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,8 +97,8 @@ const sr = ScrollReveal({
 });
 
 sr.reveal(`.home__data, .home__img, 
-           .decoration__data,
-           .accessory__content,
+           .courses__data,
+           .depositions__content,
            .footer__content`, {
     origin: 'top',
     interval: 200,
